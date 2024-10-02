48f977d5afc6a45e7f397e59b21bd8539aa4166e
Aaryan Prakash <141843008+TheAaryanPrakash@users.noreply.github.com>
2024-10-02 12:40:40 +0530

    Add files via upload

---
+++ Record/Lab-1-Stack-C.c
@@ -0,0 +1,60 @@
+//Stack Operations
+#include<stdio.h>
+#include<stdlib.h>
+int stack[10],top=-1,i,item;
+#define max 9
+
+void push(){
+    if(top==max-1){
+        printf("Stack Overflow\n");
+    }
+    else{
+        top++;
+        printf("Enter Element to Push: ");
+        scanf("%d",&item);
+        stack[top]=item;
+    }
+}
+
+int pop(){
+    if (top==-1){
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    item=stack[top];
+    top=top-1;
+    return (item);
+}
+
+void display(){
+    if (top==-1){
+        printf("Stack Empty\n");
+    }
+    else{
+        printf("The Stack is: \n");
+        for(i=top;i>-1;i--){
+            printf("%d\n",stack[i]);
+        }
+    }
+}
+
+void main(){
+    while(1){
+        int userInput;
+        printf("Enter \n1 to Push,\n2 to Pop,\n3 to Display, and \n4 to Exit\n");
+        scanf("%d",&userInput);
+        switch(userInput){
+            case 1: push();
+                break;
+            case 2: item=pop();
+                if(item!=-1){
+                    printf("The Popped Element is: %d \n",item);
+                }
+                break;
+            case 3: display();
+                break;
+            case 4: exit(0);
+                break;
+        }
+    }
+}
